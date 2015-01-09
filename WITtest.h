fa28417deceeb84a17e338c4072828d5e37c345d
Andrew Witzel <andrewwitzel@Andrews-Mac-mini.local>
2015-01-09 13:13:09 -0600

    added a test class

---
+++ WITtest.h
@@ -0,0 +1,13 @@
+//
+//  WITtest.h
+//  Git Test
+//
+//  Created by Andrew Witzel on 2015-01-09.
+//  Copyright (c) 2015 Andrew Witzel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WITtest : NSObject
+
+@end
---
+++ WITtest.m
@@ -0,0 +1,13 @@
+//
+//  WITtest.m
+//  Git Test
+//
+//  Created by Andrew Witzel on 2015-01-09.
+//  Copyright (c) 2015 Andrew Witzel. All rights reserved.
+//
+
+#import "WITtest.h"
+
+@implementation WITtest
+
+@end
