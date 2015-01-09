6e7474bdf08d9a8baa2349f9f8ffee9fc4526e93
Andrew Witzel <andrewwitzel@Andrews-Mac-mini.local>
2015-01-09 12:55:24 -0600

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Andrew Witzel on 2015-01-09.
+//  Copyright (c) 2015 Andrew Witzel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Andrew Witzel on 2015-01-09.
+//  Copyright (c) 2015 Andrew Witzel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
